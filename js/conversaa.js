fc646054cec0264c3f9bca3676ae54b77f11ff52
Fwagra <fabiocerqueiracw@gmail.com>
2016-04-01 09:31:21 +0200

    Add email to the tracking pixel

--- js/conversaa.js
+++ js/conversaa.js
@@ -2,9 +2,10 @@ jQuery.conversaaPixel = function conversaaPixel() {
   var title = escape(document.title);
   var url = window.location.href;
   var ref = document.referrer;
-  var userLang = navigator.language || navigator.userLanguage; 
-  var trackingPixel = Drupal.settings.conversaa.url + "/mtracking.gif?url=" + encodeURIComponent(url) + "&title=" + encodeURIComponent(title)+ 
-  "&language=" + encodeURIComponent(userLang)+"&referrer="+ref;
+  var userLang = navigator.language || navigator.userLanguage;
+  var email = Drupal.settings.conversaa.email;
+  var trackingPixel = Drupal.settings.conversaa.url + "/mtracking.gif?url=" + encodeURIComponent(url) + "&title=" + encodeURIComponent(title)+
+  "&language=" + encodeURIComponent(userLang)+"&referrer=" + encodeURIComponent(ref) + "&email=" + encodeURIComponent(email);
   return trackingPixel;
 }
 jQuery(document).ready(function($) {
